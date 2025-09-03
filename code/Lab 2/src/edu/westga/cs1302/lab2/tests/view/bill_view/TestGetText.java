dee960215f1f11244b95746155a7b93a94a9355b
Noah Toups <nt00063@my.westga.edu>
2025-09-03 19:54:44 -0400

    Adding Junit Tests to TestGetText
    1) Added a testGetTextWithEmptyBill to ensure it can be empty

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/bill_view/TestGetText.java
@@ -1,5 +1,26 @@
 package edu.westga.cs1302.lab2.tests.view.bill_view;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+import edu.westga.cs1302.lab2.view.BillView;
+
+import static org. junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class TestGetText {
+	
+	@Test public void testGetTextWithEmptyBill() {
+		Bill bill = new Bill();
+		BillView view = new BillView();
+		
+		String text = view.getText(bill);
+		
+		assertTrue(text.contains("ITEMS"));
+		assertTrue(text.contains("SUBTOTAL - $0.0"));
+		assertTrue(text.contains("TAX - $0.0"));
+		assertTrue(text.contains("TIP - $0.0"));
+		assertTrue(text.contains("TOTAL - $0.0"));
+	}
+	
 
 }
