6a983de895f3a7f82199efb7a3cff97e9b8fe4cc
Noah Toups <nt00063@my.westga.edu>
2025-09-03 19:39:05 -0400

    Adding JUnit Tests to TestConstructor
    1) Added a testValidConstructor to make sure it works

--- code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestConstructor.java
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/model/bill_item/TestConstructor.java
@@ -1,5 +1,19 @@
 package edu.westga.cs1302.lab2.tests.model.bill_item;
 
+import edu.westga.cs1302.lab2.model.BillItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class TestConstructor {
+	
+	@Test
+	public void testValidConstructor() {
+		BillItem item = new BillItem("Pizza", 12.5);
+		
+		assertEquals("Pizza", item.getName());
+		assertEquals(12.5, item.getAmount(), 0.0001);
+	}
+	
 
 }
