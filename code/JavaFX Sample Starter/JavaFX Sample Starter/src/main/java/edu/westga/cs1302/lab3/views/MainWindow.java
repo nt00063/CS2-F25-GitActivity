63225d966d78d5bf0339b8ae492014889cca8c7e
Noah Toups <nt00063@my.westga.edu>
2025-09-10 21:56:34 -0400

    Creating fxml class
    1) Used Scenebuilder to create the desired window
    2) Added the proper code to MainWindow.java

--- code/JavaFX Sample Starter/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,5 +1,9 @@
 package edu.westga.cs1302.lab3.views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,6 +12,20 @@ package edu.westga.cs1302.lab3.views;
  * @version Fall 2025
  */
 public class MainWindow {
+	
+	 @FXML
+	    private TextArea bill;
+
+	    @FXML
+	    private TextField itemname;
+
+	    @FXML
+	    private TextField itemprice;
+
+	    @FXML
+	    void additem(ActionEvent event) {
+
+	    }
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
